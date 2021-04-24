ba53765c10b9c12aa1acb42100165ccad2085194
Hayden Higginbotham <38926121+haydenhigg@users.noreply.github.com>
2021-04-24 10:18:02 -0400

    uint8_t -> u_int8_t

--- image.h
+++ image.h
@@ -10,10 +10,10 @@ Color getHue(double);
  * but with each raw count scaled and replaced by a color
  * array ([r, g, b]).
  */
-void makeColorMap(int w, int h, int[h][w], uint8_t[h][w][3]);
+void makeColorMap(int w, int h, int[h][w], u_int8_t[h][w][3]);
 
 /** 
  * Writes the 3D array made by `makeColorMap` to a file in
  * PPM format.
  */
-int writeToPPMFile(int w, int h, uint8_t[h][w][3], const char*);
+int writeToPPMFile(int w, int h, u_int8_t[h][w][3], const char*);
