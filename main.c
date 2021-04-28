1b38da66c2f41932f96bec0b534f70f18e2c3464
Hayden Higginbotham <38926121+haydenhigg@users.noreply.github.com>
2021-04-28 09:08:33 -0400

    make output file name a constant

--- main.c
+++ main.c
@@ -2,25 +2,25 @@
 
 #include "external.c"
 
-#define ITERS 50000000
-#define WIDTH 4000
-#define HEIGHT 4000
+#define ITERS	50000000
+#define WIDTH	4000
+#define HEIGHT	4000
+#define OUTFILE	"out.ppm"
 
 static Point attractor[ITERS];
 static u_int8_t histogram[HEIGHT][WIDTH];
 static u_int8_t colorMap[HEIGHT][WIDTH][3];
 
-int main(int argc, char **argv) {
+int main(void) {
 	// 2.01, 2.53, 1.61, -0.33
 	// -2, -2, -1.2, 2
 	// 0.56, -5.6, -1.9, 2
 	
 	AttractorParams params = newParams(2.01, 2.53, 1.61, -0.33);
-	char *outfile = argc > 1 ? argv[1] : "out.ppm";
 
 	generate(ITERS, attractor, params);
 	makeHistogram(ITERS, attractor, WIDTH, HEIGHT, histogram);
 	makeColorMap(WIDTH, HEIGHT, histogram, colorMap);
 
-	return writeToPPMFile(WIDTH, HEIGHT, colorMap, outfile);
+	return writeToPPMFile(WIDTH, HEIGHT, colorMap, OUTFILE);
 }
